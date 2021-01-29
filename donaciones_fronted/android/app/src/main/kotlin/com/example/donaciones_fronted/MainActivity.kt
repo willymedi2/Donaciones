824519ed475c83eaf792428ad2412a2dc1b535db
William <wsmedina@espol.edu.ec>
2021-01-29 11:27:22 -0500

    Fronted comenzado

---
+++ donaciones_fronted/android/app/src/main/kotlin/com/example/donaciones_fronted/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.donaciones_fronted
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ donaciones_fronted/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:donaciones_fronted/src/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginPage.id,
+      routes: {
+        LoginPage.id: (context) => LoginPage(),
+      },
+    );
+  }
+}
