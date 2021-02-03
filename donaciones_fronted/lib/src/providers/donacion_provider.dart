0fb33d915b6bcc13d836385a99cfd07d0031a662
William <wsmedina@espol.edu.ec>
2021-02-03 13:27:01 -0500

    Conflicto 2 arreglado

--- donaciones_fronted/lib/src/providers/donacion_provider.dart
+++ donaciones_fronted/lib/src/providers/donacion_provider.dart
@@ -4,11 +4,7 @@ import 'package:donaciones_fronted/src/models/donacion_model.dart';
 import 'package:http/http.dart' as http;
 
 class DonacionProvider {
-<<<<<<< HEAD
-  // final String _url = '';
-=======
   final String url = 'http://10.0.2.2:8000/donaciones/';
->>>>>>> 488e98003a74ebea3a4ac05029dda83244c5c525
 
   Future<bool> crearDonacion(DonacionModel donacion) async {
     //final url = '';
@@ -21,17 +17,10 @@ class DonacionProvider {
   }
 
   Future<List<DonacionModel>> cargarDonaciones() async {
-<<<<<<< HEAD
-    final url = "http://192.168.0.108:8000/donaciones/";
-    final resp = await http.get(url);
-
-    final List<String, dynamic> decodedData = json.decode(resp.body);
-=======
     //final url = "";
     final resp = await http.get(url);
 
     final List<dynamic> decodedData = json.decode(resp.body);
->>>>>>> 488e98003a74ebea3a4ac05029dda83244c5c525
     final List<DonacionModel> donaciones = new List();
 
     if (decodedData == null) return [];
