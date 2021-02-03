175c8978a271c43b68eb191ba16f162b7358ef4a
Bryan Plaza <bryanplaza_@hotmail.com>
2021-02-03 01:13:46 -0600

    Fix Preferencias

--- donaciones_fronted/lib/main.dart
+++ donaciones_fronted/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final prefs = new PreferenciasUsuario();
   await prefs.initPrefs();
   runApp(MyApp());
